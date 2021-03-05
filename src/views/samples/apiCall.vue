98862bc35fedbe81c43c4f1395d9c4e4ff37b013
youngsunWoo <dnaodtjs@gmail.com>
2021-03-05 12:35:16 +0900

    api call test code done

--- src/views/samples/apiCall.vue
+++ src/views/samples/apiCall.vue
@@ -1,31 +1,38 @@
 <template>
   <div class="sample-container">
     <h1> Sample View for Api call</h1>
-      <button @click="getAllUsers">테스트</button>
+      <input v-model="id" />
+      <button @click="clickEvent">UserInfo</button>
   </div>
 </template>
 
 <script>
-import {getUsers} from '@/api/users'
+import {getUsers,getUserById} from '@/api/users'
 
 export default {
   name: 'apiCallSample',
-  method: {
-    getAllUsers(){
-
-      console.log(this.form)
-      alert('submit!')
-
-      // getUsers()
-      // .then(r => {
-      //   console.log(r) 
-      // })
+  data() {
+    return {
+      id : ''
     }
   },
-  beforeMount (){ //동적 호출 
-      getUsers().then(r => {console.log(r) })
-  }
+  methods : {
+    clickEvent() {
+      if (!this.id){
+          getUsers().then(r => {
+            //TODO 
+            //Do something after api call Processe
+            console.log(r)
+            });
+      } else {
+          getUserById(this.id).then(r => {
+            //Do something after api call Processe
+            console.log(r)
+            });
+      }
 
+    },
+  },
 }
   
 </script>
